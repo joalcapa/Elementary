3d436ef17cd9dffb2b2de72b07be73689e1ce9fa
Jose Caceres <josecaceres.oreul@gmail.com>
2018-12-05 15:06:58 -0400

    Create Constants.php

---
+++ src/Config/Constants.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Joalcapa\Elementary\Config;
+
+class Constants {
+
+    const CREATE = 'create';
+
+    const MODEL = 'model';
+    const CONTROLLER = 'controller';
+    const MIDDLEWARE = 'middleware';
+    const MIGRATION = 'migration';
+
+    const MIGRATE = 'migrate';
+}
