571569a99c2170a3c02b0323309e1bc72d1c8ba4
Jose Caceres <josecaceres.oreul@gmail.com>
2019-02-16 16:11:09 +0100

    TypeAttrQ

---
+++ src/Generics/TypeAttrQ.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Joalcapa\Elementary\Generics;
+
+class TypeAttrQ {
+    const STRING = 'string';
+    const INTEGER = 'integer';
+    const DOUBLE = 'double';
+    const CHAR = 'char';
+    const BOOLEAN = 'boolean';
+    const FLOAT = 'float';
+}
